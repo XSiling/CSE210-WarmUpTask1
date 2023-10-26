dad49369a26957a8b83569957d371f22d13a0726
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-10-25 19:24:44 -0700

    update the files

--- old/src/stretchtext.js
+++ old/src/stretchtext.js
@@ -1,120 +1,92 @@
-(function(){
-	'use strict';
+"use strict";
 
-	var TITLE_WHEN_CLOSED = 'Expand';
-	var TITLE_WHEN_OPEN = 'Collapse';
+class StretchText {
+  constructor() {
+    this.TITLE_WHEN_CLOSED = "Expand";
+    this.TITLE_WHEN_OPEN = "Collapse";
+    this.toggleSummary = this.toggleSummary.bind(this);
+    this.init();
+  }
 
-	// requestAnimationFrame shimming.
-	var requestAnimationFrame =
-		window.requestAnimationFrame       ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function(callback){
-        	window.setTimeout(callback, 1000 / 60);
-        };
+  init() {
+    document.addEventListener("DOMContentLoaded", () => {
+      this.getSummaries().forEach((summary) => {
+        this.setupSummary(summary);
+      });
+    });
+  }
 
-	function toggleSummary(evt){
-		// Prevent the text from being selected if rapidly clicked.
-		evt.preventDefault();
+  setupSummary(summary) {
+    summary.setAttribute("title", this.TITLE_WHEN_CLOSED);
+    summary.addEventListener("mousedown", this.toggleSummary);
+    summary.addEventListener("touchstart", this.toggleSummary);
+    summary.addEventListener("click", (e) => e.preventDefault());
+  }
 
-		var summary = evt.target;
-		var detail = findDetailFor(summary);
-		if (!detail){ return; }
+  requestAnimationFrame(callback) {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      (cb => window.setTimeout(cb, 1000 / 60))
+    )(callback);
+  }
 
-		// CSS Transitions don't work as expected on things set to 'display: none'. Make the
-		// stretch details visible if needed, then use a timeout for the transition to take
-		// effect.
-		if (summary.classList.contains('stretchtext-open')){
-			detail.style.display = 'none';
-		} else {
-			detail.style.display = isBlockLevelDetail(summary) ? 'block' : 'inline';
-		}
+  toggleSummary(evt) {
+    evt.preventDefault();
+    
+    const summary = evt.target;
+    const detail = this.findDetailFor(summary);
+    if (!detail) return;
 
-		requestAnimationFrame(function(){
-			summary.classList.toggle('stretchtext-open');
-			detail.classList.toggle('stretchtext-open');
+    const shouldDisplay = summary.classList.contains("stretchtext-open")
+      ? "none"
+      : this.isBlockLevelDetail(summary) ? "block" : "inline";
+    detail.style.display = shouldDisplay;
 
-			if (summary.classList.contains('stretchtext-open')){
-				setTitle(summary, TITLE_WHEN_OPEN);
-			} else {
-				setTitle(summary, TITLE_WHEN_CLOSED);
-			}
-		});
-	}
+    this.requestAnimationFrame(() => {
+      summary.classList.toggle("stretchtext-open");
+      detail.classList.toggle("stretchtext-open");
+      const title = summary.classList.contains("stretchtext-open")
+        ? this.TITLE_WHEN_OPEN
+        : this.TITLE_WHEN_CLOSED;
+      this.setTitle(summary, title);
+    });
+  }
 
-	function isBlockLevelDetail(summary){
-		return summary.nodeName.toLowerCase() === 'a';
-	}
+  isBlockLevelDetail(summary) {
+    return summary.nodeName.toLowerCase() === "a";
+  }
 
-	function setTitle(summary, title){
-		// If the user placed a manual title on the summary leave it alone.
-		if (summary.hasAttribute('title')){
-			return;
-		} else {
-			summary.setAttribute('title', title);
-		}
-	}
+  setTitle(summary, title) {
+    if (!summary.hasAttribute("title")) {
+      summary.setAttribute("title", title);
+    }
+  }
 
-	function findDetailFor(summary){
-		if (isBlockLevelDetail(summary)){
-			var id = summary.getAttribute('href').replace(/^#/, '');
-			var detail = document.getElementById(id);
-			if (!detail && window.console){
-				console.error('No StretchText details element with ID: ' + id);
-			}
-			return detail;
-		} else {
-			var detail = summary.nextElementSibling;
-			if (!detail && window.console){
-				console.error('No StretchText details element found for: ', summary);
-			}
-			return detail;
-		}
-	}
+  findDetailFor(summary) {
+    if (this.isBlockLevelDetail(summary)) {
+      const id = summary.getAttribute("href").replace(/^#/, "");
+      const detail = document.getElementById(id);
+      if (!detail)
+        console.error(`No StretchText details element with ID: ${id}`);
+      return detail;
+    } else {
+      const detail = summary.nextElementSibling;
+      if (!detail)
+        console.error("No StretchText details element found for: ", summary);
+      return detail;
+    }
+  }
 
-	function getSummaries(){
-		var results = [],
-			summaries;
+  getSummaries() {
+    return [
+      ...document.querySelectorAll('[epub-type="stretchsummary"]'),
+      ...document.getElementsByClassName("stretchsummary"),
+    ];
+  }
+}
 
-		// epub-type
-		summaries = document.querySelectorAll('[epub-type="stretchsummary"]');
-		Array.prototype.forEach.call(summaries, function(result){
-			results.push(result);
-		});
-
-		// CSS class.
-		summaries = document.getElementsByClassName('stretchsummary');
-		Array.prototype.forEach.call(summaries, function(result){
-			results.push(result);
-		});
-
-		return results;
-	}
-
-	var loadedCalled = false;
-	function loaded(){
-		if (loadedCalled){ return; }
-		loadedCalled = true;
-		// FIXME(slightlyoff): Add global handlers instead of one per item.
-		getSummaries().forEach(function(summary){
-			summary.setAttribute('title', TITLE_WHEN_CLOSED);
-
-			// Listen on mousedown instead of click so that we can prevent text
-			// selection if mouse is clicked rapidly.
-			summary.addEventListener('mousedown', toggleSummary);
-
-			summary.addEventListener('touchstart', toggleSummary);
-
-			// Link resolving can't be canceled in mousedown event, only in click
-			// event.
-			summary.addEventListener('click', function(e){e .preventDefault(); });
-		});
-	}
-
-	window.addEventListener('DOMContentLoaded', loaded);
-	if (document.readyState == "complete"){
-		loaded();
-	}
-})();
+new StretchText();
